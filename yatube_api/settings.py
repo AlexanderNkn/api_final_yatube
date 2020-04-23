417b38478a8c787e1a2f4e49f98eb3c914d4fd41
Alexander <spresi@mail.ru>
2020-04-23 16:36:39 +0300

    added .env

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -1,9 +1,14 @@
 import os
+from datetime import timedelta
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
@@ -94,3 +99,8 @@ REST_FRAMEWORK = {
             'rest_framework_simplejwt.authentication.JWTAuthentication',
         ],
     }
+
+# set token lifetime
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
+}
